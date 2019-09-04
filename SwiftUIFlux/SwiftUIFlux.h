6df4749633aa16201193e1a94176617f57185594
Alexandre Morgado <alexandremorgado@gmail.com>
2019-09-04 17:22:22 -0300

    Added Carthage support

---
+++ SwiftUIFlux/SwiftUIFlux.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUIFlux.h
+//  SwiftUIFlux
+//
+//  Created by Alexandre Morgado on 04/09/19.
+//  Copyright © 2019 Alexandre Morgado. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIFlux.
+FOUNDATION_EXPORT double SwiftUIFluxVersionNumber;
+
+//! Project version string for SwiftUIFlux.
+FOUNDATION_EXPORT const unsigned char SwiftUIFluxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIFlux/PublicHeader.h>
+
+
